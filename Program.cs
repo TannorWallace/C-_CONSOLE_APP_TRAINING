e9d0be39be0f634a001c9520ad5e2eb9d52c11dc
TannorWallace <tannor.wallace@gmail.com>
2019-12-30 20:38:43 -0700

    C# after the holidays hopping back on the train

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Console_Lesson
+{
+  class Program
+  {
+    static void Main(string[] args)
+    //Method 1
+    // {
+    //   Console.WriteLine("Please enter a message!");
+    //   System.Console.WriteLine("You said..." + Console.ReadLine());
+
+    // }
+    //Method 2
+    {
+      Console.WriteLine("What is the name of your band?");
+      string name = Console.ReadLine();
+      Console.WriteLine("How many members are in your band?");
+      int members = 0;
+      if (!int.TryParse(Console.ReadLine(), out members))
+      {
+        Console.WriteLine("Enter a number dude.");
+        Environment.Exit(0);
+      }
+      if (members < 1)
+      {
+        Console.WriteLine("You must have atleast 1 member");
+        Environment.Exit(0);
+      }
+      else if (members == 1)
+        Console.WriteLine(name + " is a solo act");
+      else if (members == 2)
+        Console.WriteLine(name + " is a duo act");
+      else
+        Console.WriteLine(name + " has " + members + " members.");
+
+    }
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 "# C-_CONSOLE_APP_TRAINING" 
+
+classes allow us to separate our code in to objects 
+
+methods contain the actual executable code of the object
+
+dotnet restore needs to ne run every time we change rhe dependecies or before the first time the program runs
